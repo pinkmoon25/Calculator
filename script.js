d65f93a5879155de4c69ea1904b115893f8efb45
Tushar <singhtushar348@gmail.com>
2022-07-17 20:29:53 +0100

    Add switch in operate function

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const display = document.querySelector('.input-display');
 const numBtn = document.querySelectorAll('input');
 const clearBtn = document.querySelector('.clear');
 const result = document.querySelector('.result-display');
-const operators = document.querySelectorAll('.operators > *');
+const operators = document.querySelector('.operators');
+const equalBtn = document.querySelector('.equal');
 
 let store; 
 
@@ -11,7 +12,6 @@ function displayInput(){
     display.innerText += btn.value;
     store = display.innerText;
   })});
-  display.innerText += display.innerText;
 };
 
 displayInput();
@@ -36,16 +36,37 @@ function divide(x, y){
   return parseInt(x) / parseInt(y);
 };
 
-function operate(){
+function operate(operator){
   let val;
   for(let i=0; i<store.length; i++){
-    if(store[i] === '+') {
+    switch(store[i].charCodeAt()){
+      case 43:
       val = store.split('+');
+      if(val[0] === '' || val[1] === '') return;
+      result.innerText = `${add(val[0], val[1])}`;
+      display.innerText = `${add(val[0], val[1])}`;
+      break;
+      case 8722:
+      val = store.split('−');
+      if(val[0] === '' || val[1] === '') return;
+      result.innerText = `${subtract(val[0], val[1])}`;
+      display.innerText = `${subtract(val[0], val[1])}`;
+      break;
+      case 215:
+      val = store.split('×');
+      if(val[0] === '' || val[1] === '') return;
+      result.innerText = `${multiply(val[0], val[1])}`;
+      display.innerText = `${multiply(val[0], val[1])}`;
+      break;
+      case 247:
+      val = store.split('÷');
+      if(val[0] === '' || val[1] === '') return;
+      result.innerText = `${divide(val[0], val[1])}`;
+      display.innerText = `${divide(val[0], val[1])}`;
+      break; 
     }
-  }
-  result.innerText = `${add(val[0], val[1])}`;  
+  }  
 };
 
-operators.forEach((operator) => {
-  operator.addEventListener('click', operate)
-});
+operators.addEventListener('click', operate);
+equalBtn.addEventListener('click', operate);
